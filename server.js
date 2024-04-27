365dcc54a1f08d63f93eb8e307a631f5f0ad2434
RaveeshaSashini <raveeshasashini@gmail.com>
2024-04-27 22:47:24 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,55 +1,53 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
-const useRouter = require('./routes/user.routes')
-const supplierRouter = require('./routes/supplier.routes')
-const itemRouter = require('./routes/web.inventory.routes')
-const itemKitRouter = require('./routes/item.kit.routes')
-const cookieParser = require('cookie-parser')
-const PORT = process.env.PORT || 8000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const useRouter = require("./routes/user.routes");
+const supplierRouter = require("./routes/supplier.routes");
+const itemRouter = require("./routes/web.inventory.routes");
+const itemKitRouter = require("./routes/item.kit.routes");
+const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT || 8000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(bodyParser.json());
-app.use(express.json()) // for parsing application/json
+app.use(express.json()); // for parsing application/json
 
 //app.use(cors({origin:"*"})) // configure CORS
 app.use(cookieParser());
 const corsOptions = {
-  origin: 'http://localhost:3000', // Update this to match your frontend's origin
-  credentials: true, // This allows the server to receive and send cookies
- };
- 
+  origin: "http://localhost:3000",
+  // This allows the server to receive and send cookies
+  credentials: true,
+};
+
 app.use(cors(corsOptions));
-
-
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
- });
-
+  res.status(500).send("Something broke!");
+});
 
 // start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-}) 
+  console.log(`Server is running on port ${PORT}`);
+});
 
 // connect to MongoDB
 mongoose.connect(MONGO_URI, {});
-const connection = mongoose.connection; 
+const connection = mongoose.connection;
 
 connection.once("open", () => {
-    console.log("Database Connection Successful");
-})
+  console.log("Database Connection Successful");
+});
 
 //Importing the route
-app.use("/supplier",supplierRouter)
-app.use("/user",useRouter);
-app.use("/item",itemRouter);
-app.use("/itemkit",itemKitRouter);
+app.use("/supplier", supplierRouter);
+app.use("/user", useRouter);
+app.use("/item", itemRouter);
+app.use("/itemkit", itemKitRouter);
 
 module.exports = app;
