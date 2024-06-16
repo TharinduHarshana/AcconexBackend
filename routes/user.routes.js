7555d5179422242976c0a8365b98465397dc2de3
RaveeshaSashini <raveeshasashini@gmail.com>
2024-06-16 10:25:14 +0530

    backend route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,4 @@
 const useRouter = require("express").Router();
-const authMiddleware = require("../Middlewares/auth.middleware");
-const roleCheck = require("../Middlewares/role.check.middleware");
-
 const { login, logout } = require("../controller/auth.controller");
 const {
   addUser,
@@ -21,47 +18,28 @@ useRouter.post("/login", login);
 useRouter.get("/logout", logout);
 
 // Route for adding a new user
-useRouter.post("/add", authMiddleware, roleCheck(["admin"]), addUser);
+useRouter.post("/add", addUser);
 
 // Route for getting all users (restricted to admin role)
-useRouter.get("/all", authMiddleware, roleCheck(["admin"]), getAllUser);
+useRouter.get("/all", getAllUser);
 
 //Route for update profile
-useRouter.patch(
-  "/profile",
-  authMiddleware,
-  updateProfile
-);
+useRouter.patch("/profile", updateProfile);
 
 //get user profile data
-useRouter.get("/edit/profile", authMiddleware, getUserProfile);
+useRouter.get("/edit/profile", getUserProfile);
 
 // Route for getting user by Id (restricted to admin role)
-useRouter.get("/:id", authMiddleware, roleCheck(["admin"]), getUserById); // Changed the parameter name to ':id'
+useRouter.get("/:id", getUserById); // Changed the parameter name to ':id'
 
 // Route for checking user by ID (restricted to admin role)
-useRouter.get(
-  "/check/:userId",
-  authMiddleware,
-  roleCheck(["admin"]),
-  checkUserId
-);
+useRouter.get("/check/:userId", checkUserId);
 
 // Route for updating a user by ID (restricted to admin role)
-useRouter.patch(
-  "/update/:id",
-  authMiddleware,
-  roleCheck(["admin"]),
-  updateUserById
-);
+useRouter.patch("/update/:id", updateUserById);
 
 // Route for deleting a user by ID (restricted to admin role)
-useRouter.delete(
-  "/delete/:_id",
-  authMiddleware,
-  roleCheck(["admin"]),
-  deleteUserById
-);
+useRouter.delete("/delete/:_id", deleteUserById);
 
 // Exporting the router
 module.exports = useRouter;
