aadd91725d885671887e110cc36985033e17acf1
Hirunisha-Nirmani <hirunishanirmani@gmail.com>
2024-09-02 20:45:47 +0530

    update inventory backend
    update the inventory backend

--- routes/inventory.routes.js
+++ routes/inventory.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const authMiddleware = require("../Middlewares/auth.middleware");
 const roleCheck = require("../Middlewares/role.check.middleware");
-const {addNewItem ,getAllItems , getItemByID , updateItemByID , deleteItemById,getItemCount} = require('../controller/inventory.controller');
+const {addNewItem ,getAllItems , getItemByID , updateItemByID , deleteItemById,getItemCount,updateItemByProductID,getItemByProductID} = require('../controller/inventory.controller');
 
 //get all items count
 router.get('/count', getItemCount);
@@ -15,12 +15,17 @@ router.get('/',getAllItems);
 //get item using ID
 router.get('/:id', getItemByID);
 
+//get item using productID
+router.get('/get/:productID', getItemByProductID);
+
 //Update item Using ID
 router.patch('/update/:id',updateItemByID);
 
 //Delete item using ID
 router.delete('/delete/:id',authMiddleware, roleCheck(["admin"]),deleteItemById)
 
+//update item by product id
+router.patch('/update/productID/:productID', updateItemByProductID);
 
 
 
